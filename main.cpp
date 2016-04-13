03fcf942455b19aaa457a2908b1461facab00d1d
Gregory <parentalbox@gmail.com>
2016-04-14 00:51:34 +0300

    (completed task**#3**)
    - added method **char countWords (char inputLine[])**
      - it parses through array given and compares each element with **delimeters** array
        - if element == delimeter - it divides words
      - returns an amount of words in integer

--- main.cpp
+++ main.cpp
@@ -4,13 +4,62 @@ char delimeters[] = {' ', '\0'};
 char line[128];
 
 int isDelimeter(char symbol);
+
 int fromConsole(char inputLine[]);
+
+int countWords(char inputLine[]);
 
 int main() {
 
-    printf("%d", fromConsole(line));
+    printf("symbols: %d\n", fromConsole(line));
+    printf("words: %d\n", countWords(line));
 
 }
+
+int countWords(char inputLine[]) {
+
+    int q = 0;
+    int words = 0;
+    int i = 0;
+
+    while (inputLine[i] != '\0') {
+        if (q == 0) {
+            if (isDelimeter(inputLine[i])) {
+                q = 1;
+                i++;
+                continue;
+            }
+            q = 2;
+            i++;
+            continue;
+        }
+        if (q == 1) {
+            if (isDelimeter(inputLine[i])) {
+                i++;
+                continue;
+            }
+            q = 2;
+            i++;
+            continue;
+        }
+        if (q == 2) {
+            if (isDelimeter(inputLine[i])) {
+                q = 1;
+                words++;
+                i++;
+                continue;
+            }
+            i++;
+            continue;
+
+        }
+    }
+    if (q == 2) {
+        words++;
+    }
+    return words;
+}
+
 int fromConsole(char inputLine[]) {
 
     int i = 0;
@@ -27,6 +76,7 @@ int fromConsole(char inputLine[]) {
     inputLine[i] = '\0';
     return i;
 }
+
 int isDelimeter(char charToCompare) {
 
     for (int i = 0; i < sizeof(delimeters); i++) {
