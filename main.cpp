75ef070fef379f483778c46869189844559726e5
Gregory <parentalbox@gmail.com>
2016-04-14 00:17:41 +0300

    (completed task**#2**)
    - added array **char line [128]** for further input stuff
    
    - added function **fromConsole** which
      - initialize given array with symbols, symbol by symbol up to 127 elements (ignores others if entered)
      - waits for '\n' to stop writing to array
      - adds '\0' to the last element
      - returns the amount of symbols inputted

--- main.cpp
+++ main.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
 
 char delimeters[] = {' ', '\0'};
+char line[128];
 
 int isDelimeter(char symbol);
+int fromConsole(char inputLine[]);
 
 int main() {
 
-    char symbol;
-    scanf("%c", &symbol);
-    if (isDelimeter(symbol)) {
-        printf("symbol IS delimeter");
-    } else printf("symbol is NOT delimeter");
+    printf("%d", fromConsole(line));
 
 }
+int fromConsole(char inputLine[]) {
 
+    int i = 0;
+    char inputChar;
+    for (i = 0; i < 127; i++) {
+
+        scanf("%c", &inputChar);
+        if (inputChar == '\n') {
+            break;
+        }
+        inputLine[i] = inputChar;
+
+    }
+    inputLine[i] = '\0';
+    return i;
+}
 int isDelimeter(char charToCompare) {
 
     for (int i = 0; i < sizeof(delimeters); i++) {
