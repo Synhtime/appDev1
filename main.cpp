3abc4216d571dbc603094c99dbd9d9a17dedc770
Gregory <parentalbox@gmail.com>
2016-04-13 23:38:40 +0300

    (completed task#1)
    - added delimeters array, which will contain various delimeters of words
    
    - added function isDelimeter(char charToCompare) that takes char and checks if it is present in delimeters array

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 
-using namespace std;
+char delimeters[] = {' ', '\0'};
+
+int isDelimeter(char symbol);
 
 int main() {
-    cout << "Hello, World!" << endl;
+
+    char symbol;
+    scanf("%c", &symbol);
+    if (isDelimeter(symbol)) {
+        printf("symbol IS delimeter");
+    } else printf("symbol is NOT delimeter");
+
+}
+
+int isDelimeter(char charToCompare) {
+
+    for (int i = 0; i < sizeof(delimeters); i++) {
+        if (charToCompare == delimeters[i]) {
+            return 1;
+        }
+    }
     return 0;
 }
